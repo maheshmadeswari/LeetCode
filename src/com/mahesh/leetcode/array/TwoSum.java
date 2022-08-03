31ff0b4f00606f4be1375cb2dc1db374936eca02
Maheshkumar Sellamuthu <smahesh@siraconsultinginc.com>
2022-08-03 21:56:46 +0530

    initial code

--- src/com/mahesh/leetcode/array/TwoSum.java
+++ src/com/mahesh/leetcode/array/TwoSum.java
@@ -1,11 +1,9 @@
 package com.mahesh.leetcode.array;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 class TwoSum {
-
     public static void main(String args[]) {
         int[] input = new int[]{2, 1, 5, 3};
         for (int i : twoSum(input, 4)) {
@@ -17,10 +15,10 @@ class TwoSum {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
-                return new int[] { map.get(nums[i]), i };
+                return new int[]{map.get(nums[i]), i};
             }
             map.put(target - nums[i], i);
         }
-        return new int[] {};
+        return new int[]{};
     }
 }
